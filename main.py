2ccbe2e5891691927f324a186994d1bb2b7eb0c5
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-09 18:06:57 +0500

    echo_bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
+from pprint import pprint
 
-TOKEN = '5661659754:AAGPPm7mkuylCre31dUmOBcXN3cgULdoqSQ'
+TOKEN = '6088934431:AAE-ZBo2fz_oykEdCO0f_YDM7saRBIvKfGc'
 
 def get_last_updates():
     """
@@ -13,10 +14,17 @@ def get_last_updates():
         str(text): Message text
         int(update_id): Telegram update id
     """
-    pass
+    r=requests.get(f'https://api.telegram.org/bot{TOKEN}/GetUpdates')
+    chat_id=r.json()['result'][-1]['message']['chat']['id']
+    text=r.json()['result'][-1]['message']['text']
+    updates_id=r.json()['result'][-1]['update_id']
 
-def send_message(chat_id, text):
-    """
+    return chat_id,text
+print(get_last_updates())
+    
+
+def send_message(chat_id,text):
+    '''
     Use this function to send text messages.
 
     Args:
@@ -24,5 +32,12 @@ def send_message(chat_id, text):
         text (str): Message text
     Returns:
         None
-    """
-    pass
+    '''
+    params={'chat_id':chat_id,'text':text}
+    r=requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage',params=params)
+
+    return text
+pprint(send_message(get_last_updates()[0],get_last_updates()[1]))
+
+
+
