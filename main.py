315477e91a8be47df99d58c4bc4e8832a6ed9751
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-10 19:43:04 +0500

    echo_bot073311

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import time
 import requests
-from pprint import pprint
-
 TOKEN = '6088934431:AAE-ZBo2fz_oykEdCO0f_YDM7saRBIvKfGc'
+
+#GET Updates
+def get_updates():
+
+    r=requests.get(f'https://api.telegram.org/bot{TOKEN}/GetUpdates')
+    data=r.json()['result']
+
+    return data
+
 
 def get_last_updates():
     """
@@ -15,13 +23,11 @@ def get_last_updates():
         int(update_id): Telegram update id
     """
     r=requests.get(f'https://api.telegram.org/bot{TOKEN}/GetUpdates')
-    chat_id=r.json()['result'][-1]['message']['chat']['id']
-    text=r.json()['result'][-1]['message']['text']
-    updates_id=r.json()['result'][-1]['update_id']
-
-    return chat_id,text
-print(get_last_updates())
-    
+    r=r.json()['result']
+    chat_id=r[-1]['message']['chat']['id']
+    text=r[-1]['message']['text']
+    updates_id=r[-1]['update_id']
+    return chat_id,text,updates_id 
 
 def send_message(chat_id,text):
     '''
@@ -33,11 +39,27 @@ def send_message(chat_id,text):
     Returns:
         None
     '''
-    params={'chat_id':chat_id,'text':text}
-    r=requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage',params=params)
+    param={'chat_id':chat_id,
+           'text':text
+          }
+    
+    r=requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage',params=param)
+    ans=r.json()
+    return ans
+last_update_id=-1
 
-    return text
-pprint(send_message(get_last_updates()[0],get_last_updates()[1]))
-
+while True:
+    x=get_updates()
+    chat_id,text,updates_id=get_last_updates()
+    #updates_id=get_last_updates(get_updates())[-1]
+    if updates_id!=last_update_id:
+        
+        print(updates_id, last_update_id)
+        if text=='/start':
+            send_message(chat_id,'Salom Aksado botga hush kelibsiz!')
+        else:
+            send_message(chat_id,text)
+        last_update_id=updates_id
+    time.sleep(2)
 
 
